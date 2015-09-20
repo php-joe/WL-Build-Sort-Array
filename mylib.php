e9cf6704e1ff5a816b2c0fa9fe3157a1f7840e0d
php-joe <joseph@schnauzerdesign.com>
2015-09-20 14:43:50 -0600

    first attempt

---
+++ mylib.php
@@ -0,0 +1,29 @@
+<?php
+
+function enterArray() {
+global $myEmptyList;
+
+if(isset($_POST['items'])) {
+$items = $_POST['items'];
+$myEmptyList = explode("," , $items);
+}
+else {
+$items = '';
+}
+
+$myEmptyList = array_filter($myEmptyList);
+echo "<br>Unsorted list:<br>";
+print_r($myEmptyList);
+echo "<br>";
+return $myEmptyList;
+
+}
+
+//Sorts array and prints it.
+
+function sortMyArray($myEmptyList){
+if(isset($_POST['items'])) {
+$sortedListArray = sort($myEmptyList);
+echo "<br><br>Sorted list:<br>";
+print_r($myEmptyList);
+}}
